1d47a5c86c812458d259945fe93bc26a63530dd6
thulanimbatha <thulanimbatha06@gmail.com>
2023-08-30 00:05:13 +0200

    version 1.1 - added dictionary data structure

--- board.py
+++ board.py
@@ -1,7 +1,18 @@
-def display_board():
-    # print board
-    print('1 | 2 | 3 \n'
-        '--------- \n'
-        '4 | 5 | 6 \n'
-        '--------- \n'
-        '7 | 8 | 9 \n')
+# each placement on the board has a position & symbol
+placement = {1: '1',
+             2: '2',
+             3: '3',
+             4: '4',
+             5: '5',
+             6: '6',
+             7: '7',
+             8: '8',
+             9: '9',}
+
+def display():
+    # print board - display using placement dictionary
+    print(f'{placement[1]} | {placement[2]} | {placement[3]} \n'
+          '--------- \n'
+          f'{placement[4]} | {placement[5]} | {placement[6]} \n'
+          '--------- \n'
+          f'{placement[7]} | {placement[8]} | {placement[9]} \n')
