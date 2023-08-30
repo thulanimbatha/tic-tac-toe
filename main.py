3536c986a4f296fc1b4d1c556c869f4422654db2
thulanimbatha <thulanimbatha06@gmail.com>
2023-08-30 11:24:37 +0200

    version 1.2 - user input taken, then board is changed based on user input

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 import board
 
+game_over = False
+
 board.display()
+while not game_over:
+      turn = input("X turn. Place your move")
+      if turn == 'q': game_over = True
+      else: 
+            board.placement[int(turn)] = 'X'
+            board.display()
+
+
+
