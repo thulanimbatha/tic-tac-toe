162142db2115434ab2eada140e742bb1f42f91f7
thulanimbatha <thulanimbatha06@gmail.com>
2023-09-06 01:10:20 +0200

    version 1.6 - cleaner code

--- board.py
+++ board.py
@@ -17,3 +17,18 @@ def display():
           '--------- \n'
           f'{placement[7]} | {placement[8]} | {placement[9]} \n')
     
+def winner():
+      # check if the 3 horizontal plays are the same
+      if (placement[1] == placement[2] == placement[3]) \
+        or (placement[4] == placement[5] == placement[6]) \
+        or (placement[7] == placement[8] == placement[9]):
+            return True
+      # check if the 3 vertical plays are the same
+      elif (placement[1] == placement[4] == placement[7]) \
+         or (placement[2] == placement[5] == placement[8]) \
+         or (placement[3] == placement[6] == placement[9]):
+            return True
+      # check if the 2 diagonal plays are the same
+      elif (placement[1] == placement[5] == placement[9]) \
+         or (placement[7] == placement[5] == placement[3]):
+            return True
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import board
+from board import display, winner, placement
 
 game_over = False
 turn_count = 0
@@ -6,40 +6,25 @@ turn_count = 0
 def player_turn():
       global game_over
       global turn_count
+      
       if turn_count % 2 == 0:
             move = input("X move. Place your move: ")
             if move == 'q': 
                   game_over = True
             else: 
-                  board.placement[int(move)] = 'X'
+                  placement[int(move)] = 'X'
                   turn_count += 1
-                  board.display()
+                  display()
       else:
             move = input("O move. Place your move: ")
             if move == 'q': 
                   game_over = True
             else: 
-                  board.placement[int(move)] = 'O'
+                  placement[int(move)] = 'O'
                   turn_count += 1
-                  board.display()
+                  display()
 
-def winner():
-      # check if the 3 horizontal plays are the same
-      if (board.placement[1] == board.placement[2] == board.placement[3]) \
-        or (board.placement[4] == board.placement[5] == board.placement[6]) \
-        or (board.placement[7] == board.placement[8] == board.placement[9]):
-            return True
-      # check if the 3 vertical plays are the same
-      elif (board.placement[1] == board.placement[4] == board.placement[7]) \
-         or (board.placement[2] == board.placement[5] == board.placement[8]) \
-         or (board.placement[3] == board.placement[6] == board.placement[9]):
-            return True
-      # check if the 2 diagonal plays are the same
-      elif (board.placement[1] == board.placement[5] == board.placement[9]) \
-         or (board.placement[7] == board.placement[5] == board.placement[3]):
-            return True
-
-board.display()
+display()
 while not game_over:
       if turn_count == 9:
             print('DRAW!')
